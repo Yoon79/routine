d0fd9e469edce60ccd4a886b712b82554510ed0b
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-07-24 18:08:16 +0900

    좀 더 픽셀화

--- routine/src/app/page.tsx
+++ routine/src/app/page.tsx
@@ -1,51 +1,60 @@
 import Link from 'next/link';
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
     return (
         <div className="font-dung flex flex-col items-center justify-center min-h-screen bg-black text-white">
             <div className="text-2xl mb-20">루틴 선택</div>
-            <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+            <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
                 <Link href="/type-a">
-                    <div className="p-5 text-center border rounded-lg h-48 flex flex-col items-center justify-center">
-                        <div className="flex items-center justify-center w-24 h-24">
-                            <Image src="/type-a.png" alt="type-a" width="100" height="100" className="max-w-full max-h-full" />
+                    <div className="relative">
+                        <div className="absolute top-1.5 left-1.5 w-full h-full bg-white"></div>
+                        <div className="relative p-5 bg-black text-white text-center border-2 h-48 flex flex-col items-center justify-center">
+                            <div className="flex items-center justify-center w-24 h-24">
+                                <Image src="/type-a.png" alt="type-a" width="100" height="100" className="max-w-full max-h-full" />
+                            </div>
+                            <div className="text-xl mt-2">억만장자</div>
+                            <div className="text-xs border-[1px] rounded-full mt-1 px-2">난이도 상</div>
                         </div>
-                        <div className="text-xl mt-2">억만장자</div>
-                        <div className="text-xs border-[1px] rounded-full mt-1 px-2">난이도 상</div>
                     </div>
                 </Link>
                 <Link href="/type-b">
-                    <div className="p-5 text-center border rounded-lg h-48 flex flex-col items-center justify-center">
-                        <div className="flex items-center justify-center w-24 h-24">
-                            <Image src="/type-b.png" alt="type-b" width="100" height="100" className="max-w-full max-h-full" />
+                    <div className="relative">
+                        <div className="absolute top-1.5 left-1.5 w-full h-full bg-white"></div>
+                        <div className="relative p-5 bg-black text-white text-center border-2 h-48 flex flex-col items-center justify-center">
+                            <div className="flex items-center justify-center w-24 h-24">
+                                <Image src="/type-b.png" alt="type-b" width="100" height="100" className="max-w-full max-h-full" />
+                            </div>
+                            <div className="text-xl mt-2">캐시</div>
+                            <div className="text-xs border-[1px] rounded-full mt-1 px-2">유포리아</div>
                         </div>
-                        <div className="text-xl mt-2">캐시</div>
-                        <div className="text-xs border-[1px] rounded-full mt-1 px-2">난이도 중</div>
                     </div>
                 </Link>
                 <Link href="/type-c">
-                    <div className="p-5 text-center border rounded-lg h-48 flex flex-col items-center justify-center">
-                        <div className="flex items-center justify-center w-24 h-24">
-                            <Image src="/type-c.png" alt="type-c" width="100" height="100" className="max-w-full max-h-full" />
+                    <div className="relative">
+                        <div className="absolute top-1.5 left-1.5 w-full h-full bg-white"></div>
+                        <div className="relative p-5 bg-black text-white text-center border-2 h-48 flex flex-col items-center justify-center">
+                            <div className="flex items-center justify-center w-24 h-24">
+                                <Image src="/type-c.png" alt="type-c" width="100" height="100" className="max-w-full max-h-full" />
+                            </div>
+                            <div className="text-xl mt-2">허버먼</div>
+                            <div className="text-xs border-[1px] rounded-full mt-1 px-2">신경과학자</div>
                         </div>
-                        <div className="text-xl mt-2">허버먼</div>
-                        <div className="text-xs border-[1px] rounded-full mt-1 px-2">난이도 하</div>
                     </div>
                 </Link>
                 <Link href="/type-d">
-                    <div className="p-5 text-center border rounded-lg h-48 flex flex-col items-center justify-center">
-                        <div className="flex items-center justify-center w-24 h-24">
-                            <Image src="/type-d.png" alt="type-d" width="100" height="100" className="max-w-full max-h-full" />
+                    <div className="relative">
+                        <div className="absolute top-1.5 left-1.5 w-full h-full bg-white"></div>
+                        <div className="relative p-5 bg-black text-white text-center border-2 h-48 flex flex-col items-center justify-center">
+                            <div className="flex items-center justify-center w-24 h-24">
+                                <Image src="/type-d.png" alt="type-d" width="100" height="100" className="max-w-full max-h-full" />
+                            </div>
+                            <div className="text-xl mt-2">백만원장자</div>
+                            <div className="text-xs border-[1px] rounded-full mt-1 px-2">난이도 최하</div>
                         </div>
-                        <div className="text-xl mt-2">백만원장자</div>
-                    <div className="text-xs border-[1px] rounded-full">난이도 최하</div>
-                </div>
-            </Link>
+                    </div>
+                </Link>
             </div>
         </div>
     );
 }
-
-
-
--- routine/src/app/type-c/page.tsx
+++ routine/src/app/type-c/page.tsx
@@ -1,39 +1,40 @@
 import Link from 'next/link';
+import '../globals.css';
 
 export default function TypeC() {
     return (
-        <div className="font-dung p-8 bg-black text-white min-h-screen flex flex-col">
-            <div className="flex mb-4">
+        <div className="font-dung background-pattern p-8 bg-black text-white min-h-screen flex flex-col">
+            <div className="flex z-10 mb-4">
                 <Link href="/" passHref>
                     <div className="bg-white text-black px-4 py-2">
                         루틴 선택
                     </div>
                 </Link>
             </div>
-            <h1 className="mt-4 text-3xl">허버먼 카테고리</h1>
-            <div className="md:grid gap-4 md:grid-cols-5 mt-4">
+            <h1 className="mt-4 z-10 text-3xl">허버먼 카테고리</h1>
+            <div className=" z-10 bg-black md:grid gap-4 md:grid-cols-5 mt-4">
                 <Link href="/type-c/category-1">
-                    <div className="p-5 text-center border rounded-lg flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
                         <div className="text-xl">아침 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-2">
-                    <div className="p-5 text-center border rounded-lg flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
                         <div className="text-xl">오전 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-3">
-                    <div className="p-5 text-center border rounded-lg flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
                         <div className="text-xl">오후 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-4">
-                    <div className="p-5 text-center border rounded-lg flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
                         <div className="text-xl">저녁 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-5">
-                    <div className="p-5 text-center border rounded-lg flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center h-18 md:h-48">
                         <div className="text-xl">밤 루틴</div>
                     </div>
                 </Link>
