2edd1bed3489b435b7b2cb7635b2407536c76fbd
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2024-08-04 23:11:20 +0900

    여백에 뒷 배경 보이게

--- src/app/type-c/page.tsx
+++ src/app/type-c/page.tsx
@@ -12,29 +12,29 @@ export default function TypeC() {
                 </Link>
             </div>
             <h1 className="mt-4 z-10 text-3xl">허버먼 카테고리</h1>
-            <div className=" z-10 bg-black md:grid gap-4 md:grid-cols-5 mt-4">
+            <div className="z-10 bg-transparent md:grid gap-4 md:grid-cols-5 mt-4">
                 <Link href="/type-c/category-1">
-                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 border-white flex flex-col items-center justify-center mb-4 h-18 md:h-48 bg-black">
                         <div className="text-xl">아침 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-2">
-                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 border-white flex flex-col items-center justify-center mb-4 h-18 md:h-48 bg-black">
                         <div className="text-xl">오전 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-3">
-                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 border-white flex flex-col items-center justify-center mb-4 h-18 md:h-48 bg-black">
                         <div className="text-xl">오후 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-4">
-                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center mb-4 h-18 md:h-48">
+                    <div className="p-5 text-center border-2 border-white flex flex-col items-center justify-center mb-4 h-18 md:h-48 bg-black">
                         <div className="text-xl">저녁 루틴</div>
                     </div>
                 </Link>
                 <Link href="/type-c/category-5">
-                    <div className="p-5 text-center border-2 flex flex-col items-center justify-center h-18 md:h-48">
+                    <div className="p-5 text-center border-2 border-white flex flex-col items-center justify-center h-18 md:h-48 bg-black">
                         <div className="text-xl">밤 루틴</div>
                     </div>
                 </Link>
